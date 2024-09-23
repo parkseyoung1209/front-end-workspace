e80c1b03e69ce27b65888c2cb104e7b9a275b97f
parkseyoung1209 <rodlsdyd4@naver.com>
2024-09-23 16:56:30 +0900

    0923

--- 5_React/2-jsx/src/App.js
+++ 5_React/2-jsx/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const setCounter = (e) => {
+    if (e.target.textContent === "+10") {
+      setCount(count + 10);
+    } else if (e.target.textContent === "-10") {
+      setCount(count - 10);
+    } else {
+      setCount(0);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Total Clicks : {count} </h1>
+      <button onClick={setCounter}>+10</button>
+      <button onClick={setCounter}>-10</button>
+      <button onClick={setCounter}>Click!</button>
     </div>
   );
-}
-
+};
 export default App;
